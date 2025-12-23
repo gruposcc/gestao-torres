c95a0d3c20f2ec8c0c9ec809afc23076b6eafe7e
berndof <bernardofb@gmail.com>
2025-12-23 08:35:58 -0300

    changes

--- src/routes/htmx/address.py
+++ src/routes/htmx/address.py
@@ -25,8 +25,17 @@ async def search(
     q: str = Query(..., min_length=3),
     geocoder: Nominatim = Depends(get_geocoder),
 ):
+    # param str featuretype: If present, restrict results to certain type of features.
+    # Allowed values: country, state, city, settlement.
     try:
-        locations = await geocoder.geocode(q, limit=5, addressdetails=True, timeout=10)
+        locations = await geocoder.geocode(
+            q,
+            limit=5,
+            addressdetails=True,
+            timeout=10,
+            country_codes="br",
+            language="pt-BR",
+        )
 
         if not locations:
             return HTMLResponse('<li class="p-2 text-gray-500">Nenhum resultado.</li>')
@@ -36,21 +45,18 @@ async def search(
 
         html_output = ""
         for loc in locations:
-            # Escapamos o endereço para não quebrar o JS
+            logger.debug(loc)
 
-            logger.debug(loc.raw)
+            lat = loc.latitude
+            lng = loc.longitude
 
-            if hasattr(loc, "address"):
-                display_name = loc.address.replace("'", "\\'")
-                lat = loc.latitude
-                lng = loc.longitude
-
-                html_output += f"""
-                <li class="p-2 hover:bg-indigo-100 cursor-pointer border-b border-gray-100 text-sm"
-                    @click="selectAddress({{lat: {lat}, lng: {lng}, address: '{display_name}'}})">
-                    <div class="font-medium text-gray-800">{display_name}</div>
-                </li>
-                """
+            f_name = loc
+            html_output += f"""
+            <li class="p-2 hover:bg-indigo-100 cursor-pointer border-b border-gray-100 text-sm"
+                @click="selectAddress({{lat: {lat}, lng: {lng}, address: '{f_name}'}})">
+                <div class="font-medium text-gray-800">{f_name}</div>
+            </li>
+            """
         return HTMLResponse(content=html_output)
     except Exception as e:
         return HTMLResponse(f'<li class="p-2 text-red-500">Erro: {str(e)}</li>')
