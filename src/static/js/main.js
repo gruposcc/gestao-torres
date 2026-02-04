3ff49528aae11de71b8d13c118884d7dc963075d
berndof <bernardofb@gmail.com>
2026-02-04 10:19:43 -0300

    watch on dev

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -8,41 +8,61 @@ import focus from '@alpinejs/focus'
 // Caution, this will import all the icons and bundle them.
 createIcons({ icons });
 
-document.body.addEventListener('htmx:afterSwap', function(event) {
+//LOG NOTIFY
+document.addEventListener('DOMContentLoaded', () => {
+
+
+  //listner para alterar o titulo da pagina 
+  window.addEventListener('updateTitle', (e) => {
+    console.debug("aoooooooooo caralho mudando titulo")
+    console.debug(e)
+    document.title = e.detail;
+  });
+
+  document.addEventListener('htmx:afterSwap', (event) => {
+
+    //evita processar o swap se nao tiver conteudo
+    if (!event.detail.target) return;
+
+    const isMainContent = event.detail.target.id == 'content' //||  event.detail.target.tagName === "BODY";
+    if (isMainContent) {
+      console.debug("aaaaaaaaaa segura to voando mas nao devo chegar aqi")
+    }
+
     // Re-renderiza todos os ícones no DOM após o htmx trocar o conteúdo.
-    // O htmx:afterSwap é disparado sempre que um swap de sucesso acontece.
-    createIcons({ icons });
+    //createIcons({ icons });
     //console.log("Lucide icons re-rendered after HTMX swap.");
+    if (typeof createIcons === "function") {
+      createIcons({icons})/
+      console.debug("Lucide reloaded porra")
+    }
 
     if (window.Alpine) {
       // Use Alpine's nextTick to ensure DOM/URL updates are finished
       Alpine.nextTick(() => { // <-- Use Alpine.nextTick()
-      // Access and update the store property
-      Alpine.store('sidebar').currentPath = window.location.pathname;
-      //console.log('HTMX Swap Completo. Novo Caminho Ativo:', Alpine.store('sidebar').currentPath);
-    });
+        // Access and update the store property
+        const newPath = window.location.pathname
+        Alpine.store('sidebar').currentPath = newPath
+
+        console.log('HTMX Swap Completo. Novo Caminho nessa merda:', newPath);
+      });
     }
 
-    // 2. Ouvir o popstate para botões Voltar/Avançar (boa prática)
-    window.addEventListener('popstate', () => {
-        currentPath = window.location.pathname;
-    });
-    
-});
+  });
 
+  //Ouvir o popstate para botões Voltar/Avançar 
+  window.addEventListener('popstate', () => {
+    //currentPath = window.location.pathname;
+    if (window.Alpine) {
+      Alpine.store('sidebar').currentPath = window.location.pathname
+    }
+  });
 
-//listner para alterar o titulo da pagina 
-document.addEventListener('updateTitle', (e) => {
-    document.title = e.detail;
-});
-
-
-//LOG NOTIFY
-document.addEventListener('DOMContentLoaded', function() {
   window.addEventListener('notify', function(event) {
       console.log('✅ EVENTO NOTIFY CAPTURADO!', event.detail);
   });
   console.log('Listener "notify" ativo.');
+
 });
 
 
